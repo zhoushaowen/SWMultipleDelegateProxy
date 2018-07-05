9633b18f11dea348de543480770aa27b0b04f5d9
zhoushaowen <348345883@qq.com>
2018-07-05 10:57:24 +0800

    modify some expression

--- SWMultipleDelegateProxy.podspec
+++ SWMultipleDelegateProxy.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
 
   s.name         = "SWMultipleDelegateProxy"
 
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
 
   s.homepage      = 'https://github.com/zhoushaowen/SWMultipleDelegateProxy'
 
--- SWMultipleDelegateProxyDemo/SWMultipleDelegateProxy/SWMultipleDelegateProxy.h
+++ SWMultipleDelegateProxyDemo/SWMultipleDelegateProxy/SWMultipleDelegateProxy.h
@@ -12,11 +12,11 @@
 @interface SWMultipleDelegateProxy : NSObject
 
 /**
- each delegate in the set must be strong referenced.
+ each delegate in the allDelegate array must be strong referenced.
  it will automatically remove duplicate object,if your set two or more same object.
  even if the object in allDelegate is a array or set or dictionary,it also can work.
  your should invoke this method before set delegate.
  */
-- (void)setAllDelegate:(NSArray *)allDelegate;
+- (void)setAllDelegate:(NSArray *_Nullable)allDelegate;
 
 @end
